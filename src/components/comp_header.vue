fd6c4f51b208cde70eeba5fc5bcd799aea6c755f
Ming-zz <184653774@qq.com>
2019-08-25 17:18:01 +0800

    添加顶部导航栏

---
+++ src/components/comp_header.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <el-container>
+      <el-header></el-header>
+      <el-main>
+        <el-row type="flex" align="middle">
+          <el-col :span="11">
+            <el-menu mode="horizontal" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+              <el-menu-item index="1" disabled><i class="el-icon-info"></i></el-menu-item>
+            </el-menu>
+          </el-col>
+          <el-col :span="2">
+            <img :height="150" src="../assets/logo.png">
+          </el-col>
+          <el-col :span="11">
+            <el-menu mode="horizontal" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+              <el-submenu index="3">
+                <template slot="title">
+                  <el-avatar shape="circle" fit="fill" size="small" src="../assets/logo.png"></el-avatar>
+                  <span v-text="username"> 用户名 </span>
+                </template>            
+                <el-menu-item index="3-1" v-text="username"> 用户名 </el-menu-item>
+                <el-menu-item index="3-2" v-text="userType"> 用户权限 </el-menu-item>
+              </el-submenu>
+              <el-menu-item index="5"><i class="el-icon-switch-button"></i><a>退出登录</a></el-menu-item>
+            </el-menu>
+          </el-col>
+        </el-row>
+      </el-main>
+      <el-footer></el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+          username : 'Ming',
+          userType : 'admin',
+        }
+    },
+    methods: {
+    }
+}
+</script>
--- src/views/Page_systemAdmin.vue
+++ src/views/Page_systemAdmin.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <compheader />
     <userList />
   </div>
 </template>
@@ -8,11 +9,13 @@
 <script>
 // @ is an alias to /src
 import userList from "@/components/comp_userList.vue";
+import compheader from "@/components/comp_header.vue";
 
 export default {
   name: 'Page_systemAdmin',
   components: {
-    userList
+    userList,
+    compheader
   }
 }
 </script>
