49168f35839764bbfb9c9f06d6e0f01813210039
ZTao-z <1124693098@qq.com>
2019-08-26 11:00:11 +0800

    修改部分常量

--- src/static/js/Config.js
+++ src/static/js/Config.js
@@ -1,15 +1,15 @@
 var CONFIG = {
     Status: {
         CREATE_SUCC: 0,  // 新建
-        SUBMIT_SUCC: 1,  // 提交申请
-        MODIFY_SUCC: 2,  // 修改
+        MODIFY_SUCC: 1,  // 修改
+        SUBMIT_SUCC: 2,  // 提交申请
         AUDIT_SUCC: 3,   // 审批
         INBOUND_SUCC: 4, // 入库
         DELETE_SUCC: 5,  // 移库
 
         CREATE_FAIL: 10,
-        SUBMIT_FAIL: 11,
-        MODIFY_FAIL: 12,
+        MODIFY_FAIL: 11,
+        SUBMIT_FAIL: 12,
         AUDIT_FAIL: 13,
         INBOND_FAIL: 14,
         DELETE_FAIL: 15
